beb840fd018284db3eea220a9d90708e5cd73c18
remillner <remillner@gmail.com>
2016-04-24 09:37:38 -0500

    Slot Machine finished Project

---
+++ Slot Machine/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Slot_Machine
+{
+    partial class GlitterKitty
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.reel1 = new System.Windows.Forms.PictureBox();
+            this.reel3 = new System.Windows.Forms.PictureBox();
+            this.reel2 = new System.Windows.Forms.PictureBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblPoints = new System.Windows.Forms.Label();
+            this.lblPlayPoints = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reel3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reel2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picLogo
+            // 
+            this.picLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picLogo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picLogo.Image = global::Slot_Machine.Properties.Resources.Glitter_Logo1;
+            this.picLogo.Location = new System.Drawing.Point(248, 12);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(518, 147);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLogo.TabIndex = 0;
+            this.picLogo.TabStop = false;
+            // 
+            // reel1
+            // 
+            this.reel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.reel1.Image = global::Slot_Machine.Properties.Resources._3pizzacat;
+            this.reel1.Location = new System.Drawing.Point(118, 179);
+            this.reel1.Name = "reel1";
+            this.reel1.Size = new System.Drawing.Size(259, 234);
+            this.reel1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.reel1.TabIndex = 1;
+            this.reel1.TabStop = false;
+            // 
+            // reel3
+            // 
+            this.reel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.reel3.Image = global::Slot_Machine.Properties.Resources._5tonguecat;
+            this.reel3.Location = new System.Drawing.Point(671, 179);
+            this.reel3.Name = "reel3";
+            this.reel3.Size = new System.Drawing.Size(259, 234);
+            this.reel3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.reel3.TabIndex = 2;
+            this.reel3.TabStop = false;
+            // 
+            // reel2
+            // 
+            this.reel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.reel2.Image = global::Slot_Machine.Properties.Resources._2grumpycat2;
+            this.reel2.Location = new System.Drawing.Point(393, 179);
+            this.reel2.Name = "reel2";
+            this.reel2.Size = new System.Drawing.Size(259, 234);
+            this.reel2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.reel2.TabIndex = 3;
+            this.reel2.TabStop = false;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackgroundImage = global::Slot_Machine.Properties.Resources.Paws;
+            this.btnPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.ForeColor = System.Drawing.Color.Black;
+            this.btnPlay.Location = new System.Drawing.Point(871, 61);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(94, 87);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "Play!";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.BackColor = System.Drawing.Color.Transparent;
+            this.lblResult.Font = new System.Drawing.Font("High Tower Text", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblResult.Location = new System.Drawing.Point(285, 455);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(499, 62);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Results!";
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.BackColor = System.Drawing.Color.Transparent;
+            this.lblPoints.Font = new System.Drawing.Font("High Tower Text", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPoints.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblPoints.Location = new System.Drawing.Point(42, 72);
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(181, 23);
+            this.lblPoints.TabIndex = 6;
+            this.lblPoints.Text = "Points: 200";
+            // 
+            // lblPlayPoints
+            // 
+            this.lblPlayPoints.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayPoints.Font = new System.Drawing.Font("High Tower Text", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayPoints.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblPlayPoints.Location = new System.Drawing.Point(803, 22);
+            this.lblPlayPoints.Name = "lblPlayPoints";
+            this.lblPlayPoints.Size = new System.Drawing.Size(208, 36);
+            this.lblPlayPoints.TabIndex = 7;
+            this.lblPlayPoints.Text = "Points to Play: 10";
+            // 
+            // GlitterKitty
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Slot_Machine.Properties.Resources.Purplecurtain;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1045, 551);
+            this.Controls.Add(this.lblPlayPoints);
+            this.Controls.Add(this.lblPoints);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.reel2);
+            this.Controls.Add(this.reel3);
+            this.Controls.Add(this.reel1);
+            this.Controls.Add(this.picLogo);
+            this.Name = "GlitterKitty";
+            this.Text = "Slot Machine - Rebecca Millner";
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reel3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reel2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.PictureBox reel1;
+        private System.Windows.Forms.PictureBox reel3;
+        private System.Windows.Forms.PictureBox reel2;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblPoints;
+        private System.Windows.Forms.Label lblPlayPoints;
+    }
+}
+
---
+++ Slot Machine/Form1.cs
@@ -0,0 +1,125 @@
+﻿using Slot_Machine.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slot_Machine
+{
+    public partial class GlitterKitty : Form
+    {
+        public GlitterKitty()
+        {
+            InitializeComponent();
+        }
+        int points = 200, spins = 0;
+        const int pointsToPlay = 10, tier1 = 200, tier2 = 150, tier3 = 100, tier4 = 70, tier5 = 50;
+
+        Image[] reelPics = { Resources._1blacktiara, Resources._1fattiara, Resources._1sparkletiara, Resources._2grumpycat, Resources._2grumpycat2, 
+                               Resources._2grumpycatpillow, Resources._3breadcat, Resources._3pizzacat, Resources._3tortillacat, Resources._4angrytiara, 
+                               Resources._4cutetiara, Resources._4greytiara,Resources._4unhappytiara, Resources._4whitetiara, Resources._4whitetiara2,
+                               Resources._5bigeyes, Resources._5blackcat, Resources._5happycat, Resources._5jeanscat, Resources._5snowcat, Resources._5tinycat,
+                               Resources._5tonguecat};
+        Random reelRnd = new Random();
+
+        //Executes when the user Presses Play
+        //Subtracts points from user, upps the spin counter, and calls Spin() and Compare() methods
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            points -= pointsToPlay;
+            lblPoints.Text = String.Format("Points: {0}", points);
+            spins++;
+
+            Spin();
+            Compare();
+        }
+
+        //Executes after Spin the compare the selected images and determine winnings 
+        //based on tier system of photos
+        private void Compare()
+        {
+            if ((reel1.Image == reelPics[15] || reel1.Image == reelPics[16] || reel1.Image == reelPics[17] || reel1.Image == reelPics[18] || reel1.Image == reelPics[19] || reel1.Image == reelPics[20] || reel1.Image == reelPics[21]) && (reel2.Image == reelPics[15] || reel2.Image == reelPics[16] || reel2.Image == reelPics[17] || reel2.Image == reelPics[18] || reel2.Image == reelPics[19] || reel2.Image == reelPics[20] || reel2.Image == reelPics[21]) && (reel3.Image == reelPics[15] || reel3.Image == reelPics[16] || reel3.Image == reelPics[17] || reel3.Image == reelPics[18] || reel3.Image == reelPics[19] || reel3.Image == reelPics[20] || reel3.Image == reelPics[21])) 
+            {
+                points += tier5;
+                lblPoints.Text = String.Format("Points: {0}", points);
+                lblResult.Text = String.Format("You Won the Tier Five Prize! \n+{0} Points", tier5);
+                if (points < 10)
+                    btnPlay.Enabled = false;
+            }
+            else if ((reel1.Image == reelPics[9] || reel1.Image == reelPics[10] || reel1.Image == reelPics[11] || reel1.Image == reelPics[12] || reel1.Image == reelPics[13] || reel1.Image == reelPics[14]) && (reel2.Image == reelPics[9] || reel2.Image == reelPics[10] || reel2.Image == reelPics[11] || reel2.Image == reelPics[12] || reel2.Image == reelPics[13] || reel2.Image == reelPics[14]) && (reel3.Image == reelPics[9] || reel3.Image == reelPics[10] || reel3.Image == reelPics[11] || reel3.Image == reelPics[12] || reel3.Image == reelPics[13] || reel3.Image == reelPics[14])) 
+            {
+                points += tier4;
+                lblPoints.Text = String.Format("Points: {0}", points);
+                lblResult.Text = String.Format("You Won the Tier Four Prize! \n+{0} Points", tier4);
+                if (points < 10)
+                    btnPlay.Enabled = false;
+            }
+            else if ((reel1.Image == reelPics[6] || reel1.Image == reelPics[7] || reel1.Image == reelPics[8]) && (reel2.Image == reelPics[6] || reel2.Image == reelPics[7] || reel2.Image == reelPics[8]) && (reel3.Image == reelPics[6] || reel3.Image == reelPics[7] || reel3.Image == reelPics[8])) 
+            {
+                points += tier3;
+                lblPoints.Text = String.Format("Points: {0}", points);
+                lblResult.Text = String.Format("You Won the Tier Three Prize! \n+{0} Points", tier3);
+                if (points < 10)
+                    btnPlay.Enabled = false;
+            }
+            else if ((reel1.Image == reelPics[3] || reel1.Image == reelPics[4] || reel1.Image == reelPics[5]) && (reel2.Image == reelPics[3] || reel2.Image == reelPics[4] || reel2.Image == reelPics[5]) && (reel3.Image == reelPics[3] || reel3.Image == reelPics[4] || reel3.Image == reelPics[5]))
+            {
+                points += tier2;
+                lblPoints.Text = String.Format("Points: {0}", points);
+                lblResult.Text = String.Format("You Won the Tier Two Prize! \n+{0} Points", tier2);
+                if (points < 10)
+                    btnPlay.Enabled = false;
+            }
+            else if ((reel1.Image == reelPics[0] || reel1.Image == reelPics[1] || reel1.Image == reelPics[2]) && (reel2.Image == reelPics[0] || reel2.Image == reelPics[1] || reel2.Image == reelPics[2]) && (reel3.Image == reelPics[0] || reel3.Image == reelPics[1] || reel3.Image == reelPics[2])) 
+            {
+                points += tier1;
+                lblPoints.Text = String.Format("Points: {0}", points);
+                lblResult.Text = String.Format("You Won the Tier One Prize! \n+{0} Points", tier1);
+                if (points < 10)
+                    btnPlay.Enabled = false;
+            }
+            else
+            {
+                lblResult.Text = "Sorry, You Lost This Round!";
+                if (points < 10)
+                    btnPlay.Enabled = false;
+            }
+        }
+
+        //Executes to spin the wheel and select new images for comparison
+        //Uses spin count to change chances of winner for a more dynamic experience
+        private void Spin()
+        {
+            if (spins <=5)
+            {
+                reel1.Image = reelPics[reelRnd.Next(9, reelPics.Length - 1)];
+                reel2.Image = reelPics[reelRnd.Next(9, reelPics.Length - 1)];
+                reel3.Image = reelPics[reelRnd.Next(9, reelPics.Length - 1)];
+            }
+            else if(spins <= 10)
+            {
+                reel1.Image = reelPics[reelRnd.Next(0, reelPics.Length - 1)];
+                reel2.Image = reelPics[reelRnd.Next(0, reelPics.Length - 1)];
+                reel3.Image = reelPics[reelRnd.Next(0, reelPics.Length - 1)];
+            }
+            else if(spins <= 15)
+            {
+                reel1.Image = reelPics[reelRnd.Next(4, reelPics.Length - 1)];
+                reel2.Image = reelPics[reelRnd.Next(4, reelPics.Length - 1)];
+                reel3.Image = reelPics[reelRnd.Next(4, reelPics.Length - 1)];                
+            }
+            else
+            {
+                reel1.Image = reelPics[reelRnd.Next(6, reelPics.Length - 1)];
+                reel2.Image = reelPics[reelRnd.Next(6, reelPics.Length - 1)];
+                reel3.Image = reelPics[reelRnd.Next(6, reelPics.Length - 1)];
+                spins = 0;
+            }
+        }
+    }
+}
